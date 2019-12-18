98a3f2f930e9ba3fc9ce46ee5415c6fb1fcfb256
KaterynaLis <votetolisitsa@gmail.com>
2019-12-18 15:41:39 +0100

    list of rooms added

--- src/components/lobby/gameroom.js
+++ src/components/lobby/gameroom.js
@@ -9,16 +9,34 @@ class GameRoom extends Component {
   stream = new EventSource(`${this.url}/stream`);
 
   state = {
-    text: ""
+    text: "",
+    gamerooms: []
   };
 
   componentDidMount() {
     this.stream.onmessage = event => {
       const { data } = event;
-
       const action = JSON.parse(data);
+      console.log("action", action);
 
-      console.log(action);
+      const { type, payload } = action;
+
+      switch (type) {
+        case "ALL_GAMEROOMS": {
+          return this.setState({
+            gamerooms: payload
+          });
+        }
+        case "NEW_GAMEROOM": {
+          const gamerooms = [...this.state.gamerooms, payload];
+
+          return this.setState({
+            gamerooms
+          });
+        }
+        default:
+          console.log("Ignore:", type);
+      }
     };
   }
 
@@ -38,25 +56,50 @@ class GameRoom extends Component {
 
   onChange = event => {
     const { value } = event.target;
-
+    console.log("onChange", value);
     this.setState({ text: value });
+  };
+
+  onClick = async gameroomId => {
+    console.log("gameroomId test:", gameroomId);
+    try {
+      const response = await superagent.put(`${this.url}/join`).send({
+        gameroomId,
+        userId: 1
+      });
+      console.log("response test:", response);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   render() {
     if (this.props.jwt === null) {
-      return <Link to="/login">Please sign up to share your rants</Link>;
+      return <Link to="/login">Please sign up to access the lobby </Link>;
     }
+
+    const { gamerooms } = this.state;
+
+    const list = gamerooms.map(gameroom => (
+      <div key={gameroom.id}>
+        {gameroom.name}
+        <button onClick={() => this.onClick(gameroom.id)}>Join</button>
+      </div>
+    ));
 
     return (
       <form onSubmit={this.onSubmit}>
         <input type="text" onChange={this.onChange} value={this.state.text} />
         <button>Submit</button>
+
+        {list}
       </form>
     );
   }
 }
 
 const mapStateToProps = reduxState => {
+  console.log("is jwt here?", reduxState.auth.jwt);
   return {
     jwt: reduxState.auth.jwt
   };
